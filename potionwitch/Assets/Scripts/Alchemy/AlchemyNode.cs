7612591318f570351293d25eef616ae5cb8a1aa2
IhorZ <iz@dresslab.com>
2022-12-12 15:00:33 +0200

    Added texture + alch nodes script

---
+++ potionwitch/Assets/Scripts/Alchemy/AlchemyNode.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+public class AlchemyNode
+{
+   private List<AlchemyNode> Children { get; }
+   private int EffectId { get; set; }
+   
+   public AlchemyNode(int effectId)
+   {
+      EffectId = effectId;
+      Children = new List<AlchemyNode>();
+   }
+   
+   public void AddChild(AlchemyNode child)
+   {
+      Children.Add(child);
+   }
+   
+   public void RemoveChild(AlchemyNode child)
+   {
+      Children.Remove(child);
+   }
+}
---
+++ potionwitch/Assets/Scripts/Camera/ScrollUV.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+
+[RequireComponent(typeof(MeshRenderer))]
+public class ScrollUV : MonoBehaviour
+{
+    private Camera _mainCamera;
+    private MeshRenderer _meshRenderer;
+    private Material _material;
+    private Vector2 _offset;
+    private void Start()
+    {
+        _mainCamera = Camera.main;
+        _meshRenderer = GetComponent<MeshRenderer>();
+        _material = _meshRenderer.material;
+        _offset = GetComponent<MeshRenderer>().material.mainTextureOffset;
+    }
+
+    void Update()
+    {
+        RepeatCameraPosition();
+    }
+
+    private void RepeatCameraPosition()
+    {
+        _offset = _material.mainTextureOffset;
+        Vector3 position = _mainCamera.transform.position;
+        _offset.x = position.x;
+        _offset.y = position.y;
+        _material.mainTextureOffset = _offset;
+    }
+}
