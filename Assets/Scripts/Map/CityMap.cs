922038c783395827eeccc0087cec7e4e9f926ffb
IhorZ <floglor033@gmail.com>
2023-07-22 18:45:05 +0300

    Map and console
    Map and console

---
+++ Assets/Scripts/Map/CityMap.cs
@@ -0,0 +1,93 @@
+using System.Collections.Generic;
+using QFSW.QC;
+using UnityEngine;
+
+namespace Map
+{
+    public class CityMap : MonoBehaviour
+    {
+        [SerializeField] private List<GameObject> _defaultScreens;
+        [SerializeField] private List<MapObject> _mapObjects;
+
+        [SerializeField] private List<GameObject> _map;
+
+
+        private List<GameObject> CurrentRooms;
+
+        private void Start()
+        {
+            //_currentRoom = _defaultScreens;
+            SetButtons();
+        }
+
+        [Command]
+        private void OpenMap()
+        {
+            ChangeRoom(_map);
+        }
+        
+
+        private void SetButtons()
+        {
+            foreach (MapObject mapObject in _mapObjects)
+            {
+                mapObject.SetInteractable(() => ChangeRoom(mapObject.GetRooms()));
+            }
+        }
+
+        [Command]
+        private void ReturnToHouse()
+        {
+            foreach (GameObject defaultScreen in _defaultScreens)
+            {
+                defaultScreen.SetActive(true);
+            }
+        }
+
+        private void CloseHouse()
+        {
+            foreach (GameObject defaultScreen in _defaultScreens)
+            {
+                defaultScreen.SetActive(false);
+            }
+        }
+
+        private void ChangeRoom(List<GameObject> rooms)
+        {
+            CloseHouse();
+            
+            if (CurrentRooms != null)
+            {
+                SetGOs(CurrentRooms, false);
+            }
+
+            SetGOs(rooms, true);
+
+            CurrentRooms = rooms;
+        }
+
+        private static void SetGOs(List<GameObject> rooms, bool enabled)
+        {
+            foreach (GameObject room in rooms)
+            {
+                room.SetActive(enabled);
+            }
+        }
+
+        [Command]
+        private void ChangeRoom(string room)
+        {
+            foreach (MapObject mapObject in _mapObjects)
+            {
+                string roomName = mapObject.GetRoomName();
+                if (roomName.Equals(room))
+                {
+                    ChangeRoom(mapObject.GetRooms());
+                    return;
+                }
+
+                Debug.Log($"There is no room with name {room}");
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Map/MapObject.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+namespace Map
+{
+    [Serializable]
+    public class MapObject : MonoBehaviour
+    {
+        [SerializeField]
+        private Button _interactable;
+        
+        [SerializeField]
+        private List<GameObject> _rooms;
+
+        [SerializeField] private string _roomName;
+        
+
+        public void SetInteractable(UnityAction buttonEvent)
+        {
+            _interactable.onClick.AddListener(buttonEvent);
+        }
+        public string GetRoomName()
+        {
+            return _roomName;
+        }
+
+        public List<GameObject> GetRooms()
+        {
+            return _rooms;
+        }
+    }
+}
